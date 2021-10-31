16bf4c69c094432e7aec2a4eb706e0773d6800c1
TAPASWANI BEHERA <91559048+tapaswani21@users.noreply.github.com>
2021-10-30 23:31:15 -0700

    tapaswani

---
+++ task3/navbar.php
@@ -0,0 +1,20 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #C0392B;"><b>Student Manegement System</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #C0392B;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="register.php" style="color : #C0392B;"><b>Attendance Register</b></a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="attendancehistory.php" style="color : #C0392B;"><b>Attendace History</b></a>
+              </li>
+          </div>
+       </nav>
